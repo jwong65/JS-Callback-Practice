9a95b13a3111fe37c9a057ec33fd4cd3d01ee1e9
unknown <jason.wong6507@gmail.com>
2022-10-24 22:09:38 -0400

    Added direction:null

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ move(inventory).to(0, 0)
 const character = newImage('assets/green-character/static.gif')
 move(character).to(100, 250)
 
+let direction = null;
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
