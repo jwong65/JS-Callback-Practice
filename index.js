990df9d75f0eb5cf782ba5820359c86304ec2427
unknown <jason.wong6507@gmail.com>
2022-10-24 22:46:51 -0400

    Tested withArrowKeys as a function for placing the imaeg

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+move(character).withArrowKeys(100, 250)
 
 // let direction = null;
 
@@ -52,7 +52,7 @@ let y = 250;
 // setInterval(moveCharacter, 1)
 
 move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pillar.png')).withArrowKeys(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
 move(newImage('assets/crate.png')).to(150, 350)
 move(newImage('assets/well.png')).to(500, 575)
